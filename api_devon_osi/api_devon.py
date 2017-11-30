96ba798d83dab2c7b1512f1b60a107e6ffb50dd8
Brent Allen <carvetighter@gmail.com>
2017-11-30 10:35:17 -0600

    organizing the files

--- api_devon_osi/api_devon.py
+++ api_devon_osi/api_devon.py
@@ -6,9 +6,7 @@
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$# 
 
-import json
 from osisoft.pidevclub.piwebapi.pi_web_api_client import PIWebApiClient
-import numpy
 import pandas
 
 from warnings import simplefilter
@@ -31,7 +29,7 @@ class api_devon_osi():
         self._attributes = self._get_attributes(self._skid_77)
 
     def _get_attributes(self, m_skid_77):
-        dict_temp = set()
+        dict_temp = dict()
         attributes = self.client.element.get_attributes(m_skid_77.web_id, None, None, None, 
                        None, None, None, None, None, None, None, None, None)
         for item in attributes.items:
@@ -41,5 +39,5 @@ class api_devon_osi():
     def get_data(self):
         dict_temp = dict()
         for string_key in self._attributes:
-            dict_temp[string_key] = self.client.data.get_recorded_values(self._attributes[string_key], None, None, '10-nov', None, None, 100000, None, '18-oct', None)
+            dict_temp[string_key] = self.client.data.get_recorded_values(self._attributes[string_key], None, None, '10-nov', None, None, 100, None, '18-oct', None)
         return pandas.Panel(dict_temp)
---
+++ api_devon_osi/api_devon_osi.py
@@ -0,0 +1,5 @@
+from api_devon import api_devon_osi
+
+osi_api = api_devon_osi()
+pnl = osi_api.get_data()
+pass
